814605d15ee50a9052afd1ed6e1565da37b86147
Hussam Abdellatif <33992581+hussamabdellatif@users.noreply.github.com>
2018-11-16 17:05:09 -0500

    Update Binary_Tree_Level_Order_Traversal_store_in_multidimensional_array.cpp

--- Binary_Tree_Level_Order_Traversal_store_in_multidimensional_array.cpp
+++ Binary_Tree_Level_Order_Traversal_store_in_multidimensional_array.cpp
@@ -1,3 +1,21 @@
+//Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+//For example:
+//Given binary tree [3,9,20,null,null,15,7],
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//return its level order traversal as:
+//[
+// [3],
+// [9,20],
+// [15,7]
+//]
+
+
+
 //Binary_Tree_Level_Order_Traversal_store_in_multidimensional_array.cpp
 #include<cstdio>
 #include<vector>
